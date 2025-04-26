c6fdae8fce5df4cc2d8eb6497214c19de833933d
raykaris <ryanmuthoni050@gmail.com>
2025-04-26 14:06:05 -0400

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+//typed js
+const typed = new Typed('.multi-text', {
+    strings: ['Web Developer', 'App Developer', 'Photographer'],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+ });
