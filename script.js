2fbba478d531bf621f3b99991b14528d76d9d9dd
raykaris <ryanmuthoni050@gmail.com>
2025-04-27 06:39:45 -0400

    update

--- script.js
+++ script.js
@@ -29,11 +29,3 @@ function setTheme(isDark) {
       setTheme(isDark);
     });
 
-//typed js
-const typed = new Typed('.multi-text', {
-    strings: ['Web Developer', 'App Developer', 'Photographer'],
-    typedSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
- });
